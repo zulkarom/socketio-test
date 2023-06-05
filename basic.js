e6bfda3e4f2319d9e7cd5f4ba854005dcec49480
zulkarom <zulkarom@gmail.com>
2023-06-05 17:48:33 +0800

    u

--- basic.js
+++ basic.js
@@ -4,3 +4,7 @@ const socketio = require('socket.io');
 app.use(express.static(__dirname + '/public'));
 const expressServer = app.listen(8000);
 const io = socketio(expressServer);
+
+io.on('connection', (socket) => {
+    console.log(socket.id, "has connected")
+})
